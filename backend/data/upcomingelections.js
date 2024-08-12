c20af06f71c709969a047636b7bd5deb4944fc34
Robert Brandl <74463585+robertbrandl@users.noreply.github.com>
2024-08-12 19:55:40 -0400

    Update upcomingelections.js

--- backend/data/upcomingelections.js
+++ backend/data/upcomingelections.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const getUpcomingElections = async () => {
-    let ret = await axios.get("https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyB_hmbBzUABhHbtIfVxzzxTtDpP3TG1-rw");
+    let ret = await axios.get("https://www.googleapis.com/civicinfo/v2/elections?key=");
     console.log(ret.data.elections);
     return ret.data.elections
 }
