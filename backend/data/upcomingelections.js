e7c40ecc2edad6d2b4ff7ba1b02a70b5b25ee0c8
Robert Brandl <rbrandl@stevens.edu>
2024-08-12 20:04:09 -0400

    fixed key handling

--- backend/data/upcomingelections.js
+++ backend/data/upcomingelections.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const getUpcomingElections = async () => {
-    let ret = await axios.get("https://www.googleapis.com/civicinfo/v2/elections?key=");
+    const apiKey = process.env.API_KEY;
+    let ret = await axios.get(`https://www.googleapis.com/civicinfo/v2/elections?key=${apiKey}`);
     console.log(ret.data.elections);
     return ret.data.elections
 }
